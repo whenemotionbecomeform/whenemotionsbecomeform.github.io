f4463645226b1f0d0d654b01ca62e376b64b8a22
Eun Kang <choa030@gmail.com>
2019-11-14 18:34:23 +0900

    Updates

---
+++ precache-manifest.179601f7d588019f886ee2d068323994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa3d057c7a697ad8cda7208e082cc44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f6c9b347da0767e9947",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "528b8bd154a81b58b7cb",
+    "url": "/static/js/2.1a6681cf.chunk.js"
+  },
+  {
+    "revision": "7f6c9b347da0767e9947",
+    "url": "/static/js/main.198d1185.chunk.js"
+  },
+  {
+    "revision": "5bed52be198a546c6ed9",
+    "url": "/static/js/runtime-main.4f516b70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
